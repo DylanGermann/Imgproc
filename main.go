e3b3bb4dc91a8b97a0a34415ce15467f7568f8fb
DylanGermann <dev.dyger@gmail.com>
2022-12-01 22:05:09 +0100

    feat: add CLI, channels and more, end of project

--- main.go
+++ main.go
@@ -3,11 +3,42 @@ package main
 import (
 	"Imgproc/filter"
 	"Imgproc/task"
+	"flag"
+	"fmt"
+	"os"
+	"time"
 )
 
 func main() {
-	var f filter.Filter = &filter.Grayscale{}
-	t := task.NewWaitGrpTask("./imgs", "output", f)
+	var srcDir = flag.String("src", "", "Input directory")
+	var dstDir = flag.String("dst", "", "Output directory")
+	var filterType = flag.String("filter", "grayscale", "grayscale/blur")
+	var taskType = flag.String("task", "waitgrp", "waitgrp/channel")
+	var poolSize = flag.int("poolsize", 4, "Workers pool size for the channel task")
+	flag.Parse()
 
+	var f filter.Filter
+	switch *filterType {
+	case "grayscale":
+		f = &filter.Grayscale{}
+	case "blur":
+		f = &filter.Blur{}
+	default:
+		os.Exit(1)
+	}
+
+	var t task.Tasker
+	switch *taskType {
+	case "waitgrp":
+		t = task.NewWaitGrpTask(*srcDir, *dstDir, f)
+	case "channel":
+		t = task.NewChanTask(*srcDir, *dstDir, f, *poolSize)
+	}
+
+	t := task.NewChanTask(*srcDir, *dstDir, f, 16)
+
+	start := time.Now()
 	t.Process()
+	elapsed := time.Since(start)
+	fmt.Printf("Image processing took %s\n", elapsed)
 }
--- task/task.go
+++ task/task.go
@@ -18,7 +18,7 @@ type dirCtx struct {
 }
 
 func buildFileList(srcDir string) []string {
-	files := []string{}
+	var files []string
 	fmt.Println("Generating file list...")
 	filepath.Walk(srcDir, func(path string, info os.FileInfo, err error) error {
 		if info.IsDir() || !strings.HasSuffix(path, ".jpg") {
