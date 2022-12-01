08a777494d59b19e25f0a144edf206d667e09189
DylanGermann <dev.dyger@gmail.com>
2022-12-01 18:17:29 +0100

    wip: add goroutine

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"Imgproc/filter"
+	"Imgproc/task"
+)
+
 func main() {
-	// ...
+	var f filter.Filter = &filter.Grayscale{}
+	t := task.NewWaitGrpTask("./imgs", "output", f)
+
+	t.Process()
 }
